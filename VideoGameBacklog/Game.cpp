2341a41a07d5761e020df6a557f38637ca4e6e62
Peter Lodato <rockandrollboy14@aim.com>
2017-05-29 01:54:50 -0400

    Added get and set functions for Game class, added read_file, read_one_line, and read_multiple_lines funtions in Main.cpp.

--- VideoGameBacklog/Game.cpp
+++ VideoGameBacklog/Game.cpp
@@ -2,15 +2,49 @@
 // Game.cpp
 // Created by Peter Lodato
 // Originally created 28 May 2017
-// Last modified 28 May 2017
+// Last modified 29 May 2017
 
 #include "Game.h"
 
-CGame::CGame()
-{
+using namespace std;
+
+CGame::CGame() {
 }
 
+CGame::CGame(const string new_game_name, const vector<string> new_versions, const Status new_completion_status, const int new_num_of_completions)
+:game_name(new_game_name), versions(new_versions), completion_status(new_completion_status), num_of_completions(new_num_of_completions) {}
 
-CGame::~CGame()
-{
+string CGame::get_game_name() {
+	return game_name;
+}
+
+void CGame::set_game_name(const string new_game_name) {
+	game_name = new_game_name;
+}
+
+vector<string> CGame::get_versions() {
+	return versions;
+}
+
+void CGame::set_versions(const vector<string> new_versions) {
+	versions = new_versions;
+}
+
+Status CGame::get_completion_status() {
+	return completion_status;
+}
+
+void CGame::set_completion_status(const Status new_completion_status) {
+	completion_status = new_completion_status;
+}
+
+int CGame::get_num_of_completions() {
+	return num_of_completions;
+}
+
+void CGame::set_num_of_completions(const int new_num_of_completions) {
+	num_of_completions = new_num_of_completions;
+}
+
+CGame::~CGame() {
 }
--- VideoGameBacklog/Game.h
+++ VideoGameBacklog/Game.h
@@ -2,14 +2,39 @@
 // Game.h
 // Created by Peter Lodato
 // Originally created 28 May 2017
-// Last modified 28 May 2017
+// Last modified 29 May 2017
 
 #pragma once
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class Status {
+	NOT_STARTED,
+	IN_PROGRESS,
+	COMPLETED
+};
+
 class CGame
 {
+private:
+	string game_name;
+	vector <string> versions;
+	Status completion_status;
+	int num_of_completions;
 public:
 	CGame();
+	CGame(const string new_game_name, const vector <string> new_versions, const Status new_completion_status, const int new_num_of_completions);
+	string get_game_name();
+	void set_game_name(const string new_game_name);
+	vector <string> get_versions();
+	void set_versions(const vector <string> new_versions);
+	Status get_completion_status();
+	void set_completion_status(const Status new_completion_status);
+	int get_num_of_completions();
+	void set_num_of_completions(const int new_num_of_completions);
 	~CGame();
 };
 
